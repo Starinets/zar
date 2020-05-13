102a129cffe392f46914051c0729d11897e0f04c
Starinets <tsn.ipad@gmail.com>
2020-05-14 03:51:23 +0600

    inline styles for <h1>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const AppHeader = () => {
+  const margin = 40;
+  const headerStyle = {
+    color: 'red',
+    marginLeft: `${ margin }px`,
+    marginBottom: `${ margin }px`,
+  };
+
+  return (
+    <h1 style={ headerStyle }>Hello world, React.js</h1>
+  )
+};
+
+const AppInput = () => {
+  const placeholder = 'Type text...';
+  
+  return (
+    <label htmlFor="search">
+      <input id="search" placeholder={ placeholder } />
+    </label>
+  );
+};
+
+const AppList = () => {
+  const items = ['Item1', 'Item2', 'Item3', 'Item4'];
+  const firstItem = <li>Item0</li>;
+  const isAuth = false;
+
+  return (
+    <ul>
+      { isAuth ? firstItem : null }
+      { items.map(item => <li>{ item }</li>) }
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
+    </ul>
+  );
+};
+
+const App = () => (
+  <>
+    <AppHeader />
+    <AppInput />
+    <AppList />
+    <AppHeader />
+    <AppList />
+  </>
 );
+
+ReactDOM.render(<App />, document.getElementById('root'));
