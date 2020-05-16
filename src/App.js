594041a6f168ce6b9ab2eeaea01e58ee0046c3c4
Starinets <tsn.ipad@gmail.com>
2020-05-16 23:45:34 +0600

    changing ignore for git

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World from <code>react.js</code>
+          Hello World from <code>react.js.</code>
         </p>
       </header>
     </div>
