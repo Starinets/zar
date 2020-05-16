68bb1478dc4d82c5898795dcd7d832d329a21871
Starinets <tsn.ipad@gmail.com>
2020-05-17 01:20:19 +0600

    content transfer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import HeaderBlock from './components/HeaderBlock';
+import Header from './components/Header';
+import Paragraph from './components/Paragraph';
 
 const App = () => (
   <>
+    <HeaderBlock>
+      <Header>
+        Учите слова онлайн
+      </Header>
+      <Paragraph>
+        Используйте карточки для запоминания и пополняйте активный словарный запас.
+      </Paragraph>
+    </HeaderBlock>
     <HeaderBlock
-      title="Учите слова онлайн"
-      descr="Используйте карточки для запоминания и пополняйте активный словарный запас."
-    />
-    <HeaderBlock
-      title="Нам нравится это"
       hideBackground
-    />
+    >
+      <Header>
+        Еще один заголовок
+      </Header>
+      <Paragraph>
+        Ну здорово же.
+      </Paragraph>
+    </HeaderBlock>
   </>
 );
 
